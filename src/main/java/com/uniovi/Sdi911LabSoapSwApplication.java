86aa8a65d5cf35dac151eed7539faef276ea4b48
Aique Montes Maestre <UO261072@uniovi.es>
2020-04-19 22:22:02 +0200

    First Commit

---
+++ src/main/java/com/uniovi/Sdi911LabSoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi911LabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi911LabSoapSwApplication.class, args);
+	}
+
+}
